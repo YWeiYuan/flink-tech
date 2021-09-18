df9b76ebeeb846a394994b6682f161456deee8c1
longquan.huang <longquan.huang@ucarinc.com>
2021-09-18 19:41:16 +0800

    模块：flink tech 修改点：1. 使用scala开发flink功能

---
+++ flink-start/src/main/scala/com/weiyuan/learn/flink/source/CollectionDriver.scala
@@ -0,0 +1,24 @@
+package com.weiyuan.learn.flink.source
+
+import org.apache.flink.api.scala._
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+
+/**
+ * flink的集合数据源
+ * 将自定义的集合数据作为数据源，一般用来测试或者学习。
+ *
+ * @author longquan.huang
+ * @date 2021/9/18 6:28 下午
+ * @version 1.0
+ */
+object CollectionDriver {
+
+  def main(args: Array[String]): Unit = {
+    // 创建flink实时流的执行环境对象
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    // 获取集合数据源，flink会将实时流数据封装为DataStream进行操作
+    val source = env.fromCollection(List(1, 2, 3, 4))
+    source.print()
+    env.execute("flink-start1")
+  }
+}
